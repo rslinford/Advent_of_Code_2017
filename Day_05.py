2094f4195d14d6d93479b0d75282479d71875c3e
Scott <scott@linfords.com>
2022-08-09 23:47:10 -0700

    Day 05 part one passes

---
+++ Day_05.py
@@ -0,0 +1,27 @@
+def load_puzzle_data(filename):
+    with open(filename, 'r') as f:
+        data = [int(x) for x in f.read().strip().split('\n')]
+        print(data)
+    return data
+
+
+def follow(instructions):
+    ic = 0
+    move_counter = 0
+    while True:
+        try:
+            prev_ic = ic
+            ic += instructions[ic]
+            instructions[prev_ic] += 1
+            move_counter += 1
+        except IndexError:
+            print(f"We've escaped in {move_counter} moves.")
+            break
+
+
+def part_one(filename):
+    instructions = load_puzzle_data(filename)
+    follow(instructions)
+
+
+part_one('Day_05_small_data.txt')
